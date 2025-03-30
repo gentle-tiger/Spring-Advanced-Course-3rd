2939f9c9c7e83b0f1a13fa93a1dda480b80ad549
Choi Hojin <jkhh9725@gmail.com>
2025-03-30 21:44:37 +0900

    낙관락 학습

---
+++ OptimisticLocking/src/main/java/com/example/locking/Product.java
@@ -0,0 +1,25 @@
+package com.example.locking;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.Data;
+
+@Data
+@Entity
+public class Product {
+
+  @Id
+  @GeneratedValue(strategy =  GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+  private Double price;
+
+  @Version
+  private Integer version;
+
+  private Integer quantity;
+}
---
+++ OptimisticLocking/src/main/java/com/example/locking/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.locking;
+
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
